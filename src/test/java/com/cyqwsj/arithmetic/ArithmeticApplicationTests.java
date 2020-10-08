06a81246b70d71cb7b8842b838f3db1eed33712e
EricChanXO <1459405650@qq.com>
2020-10-08 22:01:20 +0800

    feat(all):初始化项目

---
+++ src/test/java/com/cyqwsj/arithmetic/ArithmeticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyqwsj.arithmetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArithmeticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
